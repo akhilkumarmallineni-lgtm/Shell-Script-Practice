372ee6b5b373fe5f02e3f405d6543150e537d241
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-05-21 12:40:07 +0530

    added hello world

---
+++ timestamp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Display start time
+start_time=$(date)
+echo "Start Time: $start_time"
+
+# Wait for 10 seconds
+sleep 10
+
+# Display end time
+end_time=$(date)
+echo "End Time: $end_time"
