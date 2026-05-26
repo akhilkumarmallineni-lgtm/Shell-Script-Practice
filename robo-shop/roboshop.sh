c94160c6e626c519d5ff0cb93b90dc60656be3b0
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-05-26 15:13:40 +0530

    shell

--- robo-shop/roboshop.sh
+++ robo-shop/roboshop.sh
@@ -7,7 +7,7 @@ for instance in $@
 do
     echo "Launching instance: $instance"
     INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id $AMI_ID \
+        --image-id ami-0220d79f3f480ecf5 \
         --instance-type t3.micro \
         --security-groups "shell-robo-common" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
