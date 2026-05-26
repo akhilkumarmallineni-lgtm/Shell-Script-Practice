d127fd5f9e35d54732aee262f9341d133b54c3aa
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-05-26 15:37:21 +0530

    shell

--- robo-shop/roboshop.sh
+++ robo-shop/roboshop.sh
@@ -1,7 +1,45 @@
+# #!/bin/bash
+
+
+# AMI_ID="ami-0220d79f3f480ecf5"
+
+# for instance in $@
+# do
+#     echo "Launching instance: $instance"
+#     INSTANCE_ID=$(aws ec2 run-instances \
+#         --image-id ami-0220d79f3f480ecf5 \
+#         --instance-type t3.micro \
+#         --security-groups sg-0ce953b3c0a0a3428 \
+#         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+#         --query 'Instances[0].InstanceId' \
+#         --output text
+#     )
+#     echo "Instance ID: $INSTANCE_ID"
+
+#     if [ $instance == "frontend" ]; then
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+#          --query 'Reservations[*].Instances[*].PublicIpAddress' \
+#          --output text
+#         )
+#         echo "Access the frontend at: http://$IP"
+#     else
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+#          --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+#          --output text
+#         )
+#         echo "Private IP for $instance: $IP"
+#     fi
+    
+
+# done
+
+#  echo "All instances launched successfully."
+
 #!/bin/bash
 
-
 AMI_ID="ami-0220d79f3f480ecf5"
+# ZONE_ID="Z07086101C1CVP7AT2UK4" # replace with your zone ID
+# DOMAIN_NAME="daws90s.shop" # replace with your domain name
 
 for instance in $@
 do
@@ -9,7 +47,7 @@ do
     INSTANCE_ID=$(aws ec2 run-instances \
         --image-id ami-0220d79f3f480ecf5 \
         --instance-type t3.micro \
-        --security-groups sg-0ce953b3c0a0a3428 \
+        --security-groups "shell-robo-common" "roboshop-$instance" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text
@@ -21,16 +59,38 @@ do
          --query 'Reservations[*].Instances[*].PublicIpAddress' \
          --output text
         )
-        echo "Access the frontend at: http://$IP"
+        R53_RECORD="$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
          --query 'Reservations[*].Instances[*].PrivateIpAddress' \
          --output text
         )
-        echo "Private IP for $instance: $IP"
+        R53_RECORD="$instance.$DOMAIN_NAME"
     fi
     
+    #### Updating R53 Record ####
+    # aws route53 change-resource-record-sets \
+    # --hosted-zone-id $ZONE_ID \
+    # --change-batch '
+    #     {
+    #         "Comment": "Update A record to new IP",
+    #         "Changes": [
+    #             {
+    #                 "Action": "UPSERT",
+    #                 "ResourceRecordSet": {
+    #                     "Name": "'$R53_RECORD'",
+    #                     "Type": "A",
+    #                     "TTL": 1,
+    #                     "ResourceRecords": [
+    #                         {
+    #                             "Value": "'$IP'"
+    #                         }
+    #                     ]
+    #                 }
+    #             }
+    #         ]
+    #     }
+    # '
 
 done
-
- echo "All instances launched successfully."
+ 
