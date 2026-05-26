b0963530376b95a9f11a426fa27a17b6876f2e01
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-05-26 11:14:42 +0530

    added roboshop folder

--- robo-shop/roboshop.sh
+++ robo-shop/roboshop.sh
@@ -5,26 +5,32 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-        echo "Creating EC2 instance for $instance..."
-        INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id $AMI_ID \
+    echo "Launching instance: $instance"
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id ami-0220d79f3f480ecf5 \
         --instance-type t3.micro \
         --security-groups "roboshop-common" "roboshop-$instance" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text
     )
-        echo "instance ID: $INSTANCE_ID"
+    echo "Instance ID: $INSTANCE_ID"
 
-        if [ $instance == "frontend" ]; then
-            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-            --query 'Reservations[*].Instances[*].PublicIpAddress' \
-            --output text
+    if [ $instance == "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PublicIpAddress' \
+         --output text
         )
-        else
-            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-            --query 'Reservations[*].Instances[*].PrivateIpAddress' \
-            --output text
+        echo "Access the frontend at: http://$IP"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+         --output text
         )
-        fi
+        echo "Private IP for $instance: $IP"
+    fi
+    
+
 done
+
+ echo "All instances launched successfully."
